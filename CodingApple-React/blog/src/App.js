7ea587ea8fe30110b1046053f471c6f712457607
bonjin-all <bonjin.app@gmail.com>
2021-06-29 12:37:32 +0900

    Update App.js: state

--- CodingApple-React/blog/src/App.js
+++ CodingApple-React/blog/src/App.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [title, setTitle] = useState('남자 코드 추천')
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>        
       </div>
-      <img src={logo}/>
+
+      <div className="list">
+        <h3>{title}</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+
     </div>
   );
 }
