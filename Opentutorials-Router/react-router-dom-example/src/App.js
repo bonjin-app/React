4767787dbb28d8d83897f2562c8a552ab050b186
bonjin-all <bonjin.app@gmail.com>
2021-06-29 15:28:18 +0900

    Update App.js: useParams

--- Opentutorials-Router/react-router-dom-example/src/App.js
+++ Opentutorials-Router/react-router-dom-example/src/App.js
@@ -1,7 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link, NavLink } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link, NavLink, useParams } from "react-router-dom";
 
+var contents = [
+  { id: 1, title: 'HTML', description: 'HTML IS ...' },
+  { id: 2, title: 'CSS', description: 'CSS IS ...' },
+  { id: 3, title: 'JS', description: 'JS IS ...' },
+]
 
 function Home() {
   return (
@@ -13,10 +18,46 @@ function Home() {
 }
 
 function Topic() {
+  var { id } = useParams();
+  var content = {
+    title: 'Sorry',
+    description: 'Not Found',
+  }
+  for (var i = 0; i < contents.length; i++) {
+    if (contents[i].id == Number(id)) {
+      content = contents[i];
+      break;
+    }
+  }
+
+  return (
+    <div>
+      <h3>{content.title}</h3>
+      {content.description}
+    </div>
+  )
+}
+
+function TopicList() {
+  var list = [];
+  contents.forEach(e => {
+    list.push(
+      <li key={e.id}><NavLink to={`/topic/${e.id}`}>{e.title}</NavLink></li>
+    )
+  })
+
   return (
     <div>
       <h2>Topic</h2>
       Topic ...
+
+      <ul>
+        {list}
+      </ul>
+
+      <Route path="/topic/:id">
+        <Topic></Topic>
+      </Route>
     </div>
   )
 }
@@ -44,7 +85,7 @@ function App() {
 
         <Switch>
           <Route exact path="/"><Home /></Route>
-          <Route path="/topic"><Topic /></Route>
+          <Route path="/topic"><TopicList /></Route>
           <Route path="/contact"><Contact /></Route>
           <Route path="/">Not Found</Route>
         </Switch>
