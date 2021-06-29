08bc32aeb8173629c0ddfee9f6d85655d80372fa
bonjin-all <bonjin.app@gmail.com>
2021-06-29 14:43:25 +0900

    Update App.js: Router, Link, Switch

--- Opentutorials-Router/README.md
+++ Opentutorials-Router/README.md
@@ -4,3 +4,19 @@
 우선적으로 React 수업이 선행되어야 합니다.
 
 https://opentutorials.org/module/4058
+
+### React Router DOM
+npm install react-router-dom
+
+https://reactrouter.com/web/guides/quick-start
+
+http://localhost:3000/
+path="/"
+
+http://localhost:3000/topic
+path="/"
+path="/topic"
+
+- Link
+a 태그는 페이지가 리로드된다.
+Link 태그는 페이지가 리로드 되지 않고 Route를 한다.
--- Opentutorials-Router/react-router-dom-example/src/App.js
+++ Opentutorials-Router/react-router-dom-example/src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+function Topic() {
+  return (
+    <div>
+      <h2>Topic</h2>
+      Topic ...
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact ...
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>React Router DOM example</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/topic">Topic</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/topic"><Topic /></Route>
+          <Route path="/contact"><Contact /></Route>
+          <Route path="/">Not Found</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
