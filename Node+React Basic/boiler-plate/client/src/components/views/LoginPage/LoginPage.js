b337233170967bfba081c3c741246b33aa1d46e6
bonjin-all <bonjin.app@gmail.com>
2021-06-29 18:27:43 +0900

    Update Redux: Reducer, Action

--- Node+React Basic/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ Node+React Basic/boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import { React, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        // setPassword(event.target.value);
+        setPassword(event.currentTarget.value);
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        let body = {
+            email: email,
+            password: password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.success) {
+                    props.history.push('/');
+
+                } else {
+                    alert('Error');
+                }
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', 'alignItems': 'center', width: '100%', height: '100vh'
+        }}>
+            <form onSubmit={onSubmitHandler} style={{ display: 'flex', flexDirection: 'column' }}>
+                <label>Email</label>
+                <input type="email" value={email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={password} onChange={onPasswordHandler} />
+
+                <br />
+                <button>Login</button>
+
+            </form>
         </div>
     )
 }
