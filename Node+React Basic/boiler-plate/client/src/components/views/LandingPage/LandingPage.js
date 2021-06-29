402e180230c756f0a0a5becbcc148c17aa424396
bonjin-all <bonjin.app@gmail.com>
2021-06-29 19:38:05 +0900

    Update User: Logout기능 추가.

--- Node+React Basic/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ Node+React Basic/boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello').then(response => {
             console.log(response.data)
         })
     }, [])
 
+    const onLogoutHandler = () => {
+        axios.get('/api/user/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login')
+
+                } else {
+                    alert('오류가 발생하였습니다.');
+                }
+            })
+    }
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', 'alignItems': 'center', width: '100%', height: '100vh', backgroundColor: '#eaeaea'
         }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onLogoutHandler}>로그아웃</button>
         </div>
     )
 }
