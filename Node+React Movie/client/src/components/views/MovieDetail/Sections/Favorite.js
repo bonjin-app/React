0b3534caf5366ea93d15bdce479b3053828dc92d
bonjin-all <bonjin.app@gmail.com>
2021-06-30 17:54:00 +0900

    Update Favorite.js: Favorite 처리, 해제

--- Node+React Movie/client/src/components/views/MovieDetail/Sections/Favorite.js
+++ Node+React Movie/client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Button } from 'antd';
 
 function Favorite(props) {
 
@@ -11,26 +12,67 @@ function Favorite(props) {
     const moviePost = movieInfo.backdrop_path;
     const movieRunTime = movieInfo.runtime;
 
+    const [favoriteCount, setFavoriteCount] = useState(0);
+    const [favorited, setFavorited] = useState(false);
+
+    let variables = {
+        movieId: movieId,
+        userFrom: userFrom,
+        movieTitle: movieTitle,
+        moviePost: moviePost,
+        movieRunTime: movieRunTime
+    }
+
     useEffect(() => {
-        console.error("Favorite useEffect")
-        let variables = {
-            movieId,
-            userFrom
-        }
         Axios.post('/api/favorite/number', variables)
             .then(response => {
                 if (response.data.success) {
-                    console.log(response)
+                    setFavoriteCount(response.data.data)
+
                 } else {
                     alert('숫자 정보를 가져오는데 실패 했습니다.');
                 }
             })
+
+        Axios.post('/api/favorite/favorited', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavorited(response.data.data)
+
+                } else {
+                    alert('정보를 가져오는데 실패 했습니다.');
+                }
+            })
     }, [])
+
+    const onClickFavorite = () => {
+        if (favorited) {
+            Axios.post('/api/favorite/remove', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteCount(favoriteCount - 1)
+                        setFavorited(false)
+                    } else {
+                        alert('Favorite 리스트에서 지우는걸 실패했습니다.');
+                    }
+                })
+
+        } else {
+            Axios.post('/api/favorite/add', variables)
+                .then(response => {
+                    if (response.data.success) {
+                        setFavoriteCount(favoriteCount + 1)
+                        setFavorited(true)
+                    } else {
+                        alert('Favorite 리스트에서 추가하는걸 실패했습니다.');
+                    }
+                })
+        }
+    }
 
     return (
         <div>
-
-            <button>Favorite</button>
+            <Button onClick={onClickFavorite}>{favoriteCount ? 'Not Favorite' : 'Add to Favorite'} {favoriteCount}</Button>
         </div>
     )
 }
--- Node+React Movie/server/routes/favorite.js
+++ Node+React Movie/server/routes/favorite.js
@@ -18,10 +18,61 @@ router.post('/number', (req, res) => {
             // count 정보를 response
             res.status(200).json({
                 success: true,
-                favoriteCount: data.length
+                data: data.length
             })
 
         })
+})
+
+router.post('/favorited', (req, res) => {
+    Favorite.find({
+        movieId: req.body.movieId,
+        userFrom: req.body.userFrom
+    })
+        .exec((err, data) => {
+            if (err) {
+                return res.status(400).send(err)
+            }
+
+            let result = false;
+            if (data.length !== 0) {
+                result = true;
+            }
+
+            // count 정보를 response
+            res.status(200).json({
+                success: true,
+                data: result
+            })
+
+        })
+})
+
+router.post('/add', (req, res) => {
+    const favorite = Favorite(req.body);
+    favorite.save((err, data) => {
+        if (err) return res.status(400).send(err);
+
+        return res.status(200)
+            .json({
+                success: true
+            })
+    });
+})
+
+router.post('/remove', (req, res) => {
+
+    Favorite.findOneAndDelete({
+        movieId: req.body.movieId,
+        userFrom: req.body.userFrom
+    }).exec((err, data) => {
+        if (err) return res.status(400).send(err);
+
+        res.status(200).json({
+            success: true,
+            data: data
+        })
+    })
 })
 
 module.exports = router;
