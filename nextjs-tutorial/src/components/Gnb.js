cf1a010462ae33966ed3d109462b48b5671a8016
bonjin <bonjin.app@gmail.com>
2021-07-28 17:59:32 +0900

    Add Gnb.js

---
+++ nextjs-tutorial/src/components/Gnb.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Menu } from 'semantic-ui-react'
+
+
+function Gnb() {
+    const activeItem = "home";
+    return (
+        <Menu inverted>
+            <Menu.Item
+                name='home'
+                active={activeItem === 'home'}
+            />
+            <Menu.Item
+                name='messages'
+                active={activeItem === 'messages'}
+            />
+            <Menu.Item
+                name='friends'
+                active={activeItem === 'friends'}
+            />
+        </Menu>
+    )
+}
+
+export default Gnb
--- nextjs-tutorial/src/components/Top.js
+++ nextjs-tutorial/src/components/Top.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import Gnb from './Gnb'
 
 function Top() {
     return (
         <div>
             <img src="/images/angma.jpeg" alt="logo" />
             <Header as="h1">BONJIN</Header>
+            <Gnb />
         </div>
     )
 }
