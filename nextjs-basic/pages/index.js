9639a5f4071a13c2c9bd9f93f5ace440ca33d0d0
bonjin <bonjin.app@gmail.com>
2021-07-28 15:34:41 +0900

    Add server

--- nextjs-basic/pages/index.js
+++ nextjs-basic/pages/index.js
@@ -1,9 +1,41 @@
 import React from "react";
 
-export default function Home() {
+export default function Home({ posts }) {
+
+  console.log(posts)
   return (
     <div>
       <h1>Welcome to My Blog</h1>
+      <ul>
+        {posts.map((m, i) => {
+          return (
+            <li key={i}>{m.title}</li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
+
+// Fetch data on each request.pre - render for Server - side Rendering
+export const getServerSideProps = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`);
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts: posts,
+    }
+  }
+}
+
+// export const getStaticSideProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=0&_end=10`);
+//   const posts = await res.json();
+
+//   return {
+//     props: {
+//       posts: posts,
+//     }
+//   }
+// }
