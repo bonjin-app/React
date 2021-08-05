368c71441ae465d23251373e70d3750a9a337041
bonjin-all <bonjin.app@gmail.com>
2021-08-05 11:37:28 +0900

    Add NavBar

---
+++ React-Design/material-ui-table-crud/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core'
+import React from 'react'
+
+const useStyle = makeStyles({
+    header: {
+        background: '#111'
+    }
+})
+
+const NavBar = () => {
+    const classes = useStyle();
+
+    return (
+        <AppBar className={classes.header}>
+            <Toolbar>
+                <Typography component="h2">Code for Interview</Typography>
+                <Typography component="h2">All Users</Typography>
+                <Typography component="h2">Add User</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar
