117db0506494582bc7bda70f782ad2468c634940
bonjin-all <bonjin.app@gmail.com>
2021-08-05 13:29:29 +0900

    Update AllUsers

--- React-Design/material-ui-table-crud/src/components/AllUsers.js
+++ React-Design/material-ui-table-crud/src/components/AllUsers.js
@@ -1,10 +1,67 @@
-import React from 'react'
+import { makeStyles, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import React, { useEffect, useState } from 'react'
+import { getUsers } from '../Service/api'
+
+
+const useStyle = makeStyles({
+    table: {
+        width: '90%',
+        margin: '50px 0 0 50px',
+    },
+    thred: {
+        '& > *': {
+            background: '#000',
+            color: '#ffffff !important',
+            fontSize: 20,
+        }
+    },
+    row: {
+        '& > *': {
+            fontSize: 20,
+        }
+    }
+})
 
 const AllUsers = () => {
+
+    const [users, setUsers] = useState([]);
+    const classes = useStyle();
+
+    useEffect(() => {
+        _getAllUsers();
+    }, [])
+
+    const _getAllUsers = async () => {
+        const response = await getUsers();
+        console.log(response.data)
+        setUsers(response.data)
+    }
+
     return (
-        <p>
-            AllUsers
-        </p>
+        <Table className={classes.table}>
+            <TableHead>
+                <TableRow  className={classes.thred}>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {users.map((m, i) => {
+                    return (
+                        <TableRow className={classes.row}>
+                            <TableCell>{m.id}</TableCell>
+                            <TableCell>{m.name}</TableCell>
+                            <TableCell>{m.username}</TableCell>
+                            <TableCell>{m.email}</TableCell>
+                            <TableCell>{m.phone}</TableCell>
+                        </TableRow>
+                    )
+                })}
+            </TableBody>
+        </Table>
     )
 }
 
