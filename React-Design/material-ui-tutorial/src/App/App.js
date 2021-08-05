1c5c48463b8eade828e7e22d824df8e26f4b13e4
bonjin-all <bonjin.app@gmail.com>
2021-08-05 15:22:28 +0900

    Update ThemeProvider

--- React-Design/material-ui-tutorial/src/App/App.js
+++ React-Design/material-ui-tutorial/src/App/App.js
@@ -1,7 +1,23 @@
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import { createTheme, CssBaseline, makeStyles, ThemeProvider } from "@material-ui/core";
 import React from "react";
 import Header from "../components/Header";
 import SideMenu from "../components/SideMenu";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#333996',
+      light: '#3c44b126',
+    },
+    secondary: {
+      main: '#f83245',
+      light: '#f8324526',
+    },
+    background: {
+      default: '#f4f5fd',
+    },
+  }
+})
 
 const useStyle = makeStyles({
   appMain: {
@@ -14,13 +30,13 @@ function App() {
   const classes = useStyle();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline/>
       <SideMenu/ >
       <div className={classes.appMain}>
         <Header/>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
--- React-Design/material-ui-tutorial/src/components/Header.js
+++ React-Design/material-ui-tutorial/src/components/Header.js
@@ -1,37 +1,83 @@
-import { AppBar, Badge, Grid, IconButton, InputBase, Toolbar } from '@material-ui/core'
+import { AppBar, Badge, Grid, IconButton, InputBase, makeStyles, Toolbar } from '@material-ui/core'
 import {
     ChatBubbleOutline as ChatBubbleOutlineIcon,
     NotificationsNone as NotificationsNoneIcon,
     PowerSettingsNew as PowerSettingsNewIcon,
+    Search as SearchIcon,
 } from '@material-ui/icons'
 import React from 'react'
 
+const useStyle = makeStyles(theme => ({
+    root: {
+        backgroundColor: '#fff',
+    },
+    searchInput: {
+        opacity: '0.6',
+        padding: `0px ${theme.spacing(1)}px`,
+        fontSize: '0.8rem',
+        '&:hover': {
+            backgroundColor: '#f2f2f2'
+        },
+        '& .MuiSvgIcon-root': {
+            marginRight: theme.spacing(1),
+        }
+    },
+    btnRoot: {
+        backgroundColor: 'green'
+    },
+    btnLabel: {
+        backgroundColor: 'red',
+    }
+}));
+
 const Header = () => {
+    const classes = useStyle();
+
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.root}>
             <Toolbar>
-                <Grid container>
+                <Grid container
+                    alignItems="center"
+                >
                     <Grid item>
-                        <InputBase/>
+                        <InputBase
+                            className={classes.searchInput}
+                            placeholder="Search topics"
+                            startAdornment={
+                                <SearchIcon
+                                    fontSize="small"
+                                />
+                            }
+                        />
                     </Grid>
                     <Grid item sm></Grid>
                     <Grid item>
                         <IconButton>
+                            {/* classes={{
+                                root: classes.btnRoot,
+                                label: classes.btnLabel
+                            }} */}
                             <Badge
                                 badgeContent={4}
                                 color="secondary">
-                                <NotificationsNoneIcon />
+                                <NotificationsNoneIcon
+                                    fontSize="small"
+                                />
                             </Badge>
                         </IconButton>
                         <IconButton>
                             <Badge
                                 badgeContent={3}
                                 color="primary">
-                                <ChatBubbleOutlineIcon />
+                                <ChatBubbleOutlineIcon
+                                    fontSize="small"
+                                />
                             </Badge>
                         </IconButton>
                         <IconButton>
-                            <PowerSettingsNewIcon />
+                            <PowerSettingsNewIcon
+                                fontSize="small"
+                            />
                         </IconButton>
                     </Grid>
                 </Grid>
