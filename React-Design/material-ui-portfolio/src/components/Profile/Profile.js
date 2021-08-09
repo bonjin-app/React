c3b809e7269426116decb8ea7833de027b2c8d99
bonjin-all <bonjin.app@gmail.com>
2021-08-09 14:04:32 +0900

    Add Image

--- React-Design/material-ui-portfolio/src/components/Profile/Profile.js
+++ React-Design/material-ui-portfolio/src/components/Profile/Profile.js
@@ -1,9 +1,27 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import profileImage from '../../assets/images/profile.jpeg'
+
+// Style
+import './Profile.css';
 
 const Profile = () => {
     return (
-        <div>
-            Profile
+        <div className='profile container-shadow'>
+            <div className='profile-name'>
+                <Typography className='name'>GIGAS</Typography>
+                <Typography className='title'>Creative Developer</Typography>
+            </div>
+
+            <figure className='profile-image'>
+                <img src={profileImage} alt='profile-image'/>
+            </figure>
+
+            <div className='profile-information'>
+                insert timeline
+                <br />
+                <button>my button</button>
+            </div>
         </div>
     )
 }
