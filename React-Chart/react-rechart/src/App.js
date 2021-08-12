e49892e6e0950d5cdd6334630abdf532fb5c9f33
bonjin-all <bonjin.app@gmail.com>
2021-08-12 17:08:38 +0900

    Add BarChart

--- React-Chart/react-rechart/src/App.js
+++ React-Chart/react-rechart/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import AreaChartScreen from './components/AreaChartScreen';
+import BarChartScreen from './components/BarChartScreen';
 import LineChartScreen from './components/LineChartScreen';
 
 function App() {
   return (
     <>
+      <BarChartScreen />
+      ㅁㄴㅇㄹ
+
       <LineChartScreen />
       
       <AreaChartScreen />
+
     </>
   );
 }
