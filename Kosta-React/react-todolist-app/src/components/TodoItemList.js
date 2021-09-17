41a5c8600495583bacb2c66163337c2f5d8de21c
bonjin-app <bonjin.app@gmail.com>
2021-09-17 09:08:10 +0900

    Sync

---
+++ Kosta-React/react-todolist-app/src/components/TodoItemList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+class TodoItemList extends Component {
+    render() {
+        const { todos, myToggle, myRemove } = this.props;
+        return (
+            <div>
+                <TodoItem text="오늘의할일1" checked={true} />
+                <TodoItem text="오늘의할일2" checked={false} />
+            </div>
+        );
+    }
+}
+export default TodoItemList;
