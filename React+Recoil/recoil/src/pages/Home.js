732e91dee01c397de12bd85a111eb9af731d4c4e
bonjin <bonjin.app@gmail.com>
2023-05-03 14:01:13 +0900

    Update Recoil: atom, selector

---
+++ React+Recoil/recoil/src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil'
+
+const textState = atom({
+    key: 'textState',
+    default: '',
+});
+
+const charCountState = selector({
+  key: 'charCountState',
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text;
+  }
+})
+
+function Home() {
+  return (
+    <div>
+      <TextInput/>
+      <CharacterCount/>
+    </div>
+  )
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count.length}</>;
+}
+
+export default Home;
