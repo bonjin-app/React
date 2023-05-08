026a2a514748f57f421ed800e211c4b13189e6ed
bonjin <bonjin.app@gmail.com>
2023-05-08 13:01:20 +0900

    Update Layout.js: 헤더 통일

--- React+Nextjs/nextjs-fresh/app/page.js
+++ React+Nextjs/nextjs-fresh/app/page.js
@@ -1,14 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   let name = 'gigas';
 
   return (
    <div>
-    <div className="navbar">
-      <Link href={'/'}>홈</Link>
-      <Link href={'/list'}>List</Link>
-    </div>
     <h4 className="title">애플후레시</h4>
     <p className="title-sub">{name} by dev bonjin</p>
    </div>
