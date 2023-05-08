394cc144b2f46b35cfb5f73d5cfb5c70d6a2cc00
bonjin <bonjin.app@gmail.com>
2023-05-08 13:45:34 +0900

    Update next.config: 이미지 remote 정보 등록

--- React+Nextjs/nextjs-fresh/app/list/page.js
+++ React+Nextjs/nextjs-fresh/app/list/page.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import img from "/public/next.svg";
 
 export default function page() {
+  let products = ["Tomatoes", "Pesta", "Coconut"];
+
   return (
     <div>
-        <h4 className='title'>상품목록</h4>
-        <div className='food'>
-            <h4>상품1 $40</h4>
+      <h4 className="title">상품목록</h4>
+      {products.map((m) => (
+        <div className="food">
+          <Image className="food-img" src={'https://cdn.pixabay.com/photo/2011/03/16/16/01/tomatoes-5356_960_720.jpg'} width={500} height={400}/>
+          <h4>{m} $40</h4>
         </div>
-        <div className='food'>
-            <h4>상품2 $40</h4>
-        </div>
+      ))}
     </div>
-  )
+  );
 }
